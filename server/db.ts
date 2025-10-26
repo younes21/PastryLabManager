3ff33d14e982359e8aa54c7a113b513012344162
younes21 <ynswork@gmail.com>
2025-10-26 22:23:04 +0000

    wip for replit

--- server/db.ts
+++ server/db.ts
@@ -1,17 +1,17 @@
 // import { Pool, neonConfig } from '@neondatabase/serverless';
 // import { drizzle } from 'drizzle-orm/neon-serverless';
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 // import ws from "ws";
 import * as schema from "@shared/schema";
 
 // neonConfig.webSocketConstructor = ws;
-dotenv.config();
-console.log("******************");
+dotenv.config({ path: "./.env" });
+console.log("******************", process.env.DATABASE_URL);
 if (!process.env.DATABASE_URL) {
   throw new Error(
-    "DATABASE_URL must be set. Did you forget to provision a database?"
+    "DATABASE_URL must be set. Did you forget to provision a database?",
   );
 }
 
