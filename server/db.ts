1c5b15ef417ea373a08f413675f58f489469de4c
younes21 <ynswork@gmail.com>
2025-11-04 17:15:48 +0000

    update db

--- server/db.ts
+++ server/db.ts
@@ -8,7 +8,7 @@ import * as schema from "@shared/schema";
 
 delete process.env.DATABASE_URL;
 // neonConfig.webSocketConstructor = ws;
-dotenv.config({ path: "./.env", debug: true });
+dotenv.config();
 console.log("******************", process.env.DATABASE_URL);
 if (!process.env.DATABASE_URL) {
   throw new Error(
